0de227a93c284de9ea7dfb8f8d222cd9050083b2
Gopal sharma <gopal151295@gmail.com>
2020-04-28 23:26:10 +0530

    Naive bayes | email classify

--- NaiveBayes.py
+++ NaiveBayes.py
@@ -14,10 +14,7 @@ def loadDataSet():
     classVec = [0,1,0,1,0,1] #1 is abusive, 0 
     return postingList,classVec
 
-"""
- will create a list of all the unique words in all
-of our documents. 
-"""
+""" will create a list of all the unique words in all of our documents. """
 def createVocabList(dataSet):
     vocabSet = set([])
     for document in dataSet:
@@ -25,7 +22,8 @@ def createVocabList(dataSet):
     return list(vocabSet)
 
 """
-which takes the vocabulary list and a document and outputs a vector of 1s and 0s to represent whether a word from our vocabulary is present or not in
+which takes the vocabulary list and a document and outputs a vector 
+of 1s and 0s to represent whether a word from our vocabulary is present or not in
 the given document
 """
 def setOfWords2Vec(vocabList, inputSet):
@@ -37,13 +35,20 @@ def setOfWords2Vec(vocabList, inputSet):
             print("the word: {} not in vocab".format(word))
     return returnVec
 
+def bagOfWords2VecMN(vocabList, inputSet):
+    returnVec = [0]*len(vocabList)
+    for word in inputSet:
+        if word in vocabList:
+            returnVec[vocabList.index(word)] += 1
+    return returnVec
+
 import numpy as np
 def trainNB0(trainMatrix,trainCategory):
     numTrainDocs = len(trainMatrix)
     numWords = len(trainMatrix[0])
     pAbusive = sum(trainCategory)/float(numTrainDocs)
-    p0Num = np.zeros(numWords)
-    p1Num = np.zeros(numWords)
+    p0Num = np.ones(numWords)
+    p1Num = np.ones(numWords)
     p0Denom = 2.0
     p1Denom = 2.0
     for i in range(numTrainDocs):
@@ -53,51 +58,83 @@ def trainNB0(trainMatrix,trainCategory):
         else:
             p0Num += trainMatrix[i]
             p0Denom += sum(trainMatrix[i])
-    #p1Vect = np.log(p1Num/p1Denom)
-    #p0Vect = np.log(p0Num/p0Denom)
-    p1Vect = p1Num/p1Denom
-    p0Vect = p0Num/p0Denom
+    p1Vect = np.log(p1Num/p1Denom)
+    p0Vect = np.log(p0Num/p0Denom)
     return p0Vect,p1Vect,pAbusive
 
-""" Load dataset """
-postingList,classVec = loadDataSet()
-
-""" Create vocab lit of the loaded dataset """
-vocabList = createVocabList(postingList)
-
-""" word vector """
-wordVec0 = setOfWords2Vec(vocabList, postingList[0]) 
-
-# Train: calculating probabilities from word vectors
-trainMat = []
-for postinDoc in postingList:
-    trainMat.append(setOfWords2Vec(vocabList, postinDoc))
-    
-
-""" Now let’s get the probabilities of being abusive and the two probability vectors """
-p0V,p1V,pAb=trainNB0(trainMat,classVec)
-
 def classifyNB(vec2Classify, p0Vec, p1Vec, pClass1):
-    p1 = np.sum(vec2Classify * p1Vec) + np.log(pClass1)
-    p0 = np.sum(vec2Classify * p0Vec) + np.log(1.0 - pClass1)
+    p1 = sum(vec2Classify * p1Vec) + np.log(pClass1)
+    p0 = sum(vec2Classify * p0Vec) + np.log(1.0 - pClass1)
     if p1 > p0:
         return 1
     else:
         return 0
         
 def testingNB():
+    """ Load dataset """
     listOPosts,listClasses = loadDataSet()
+    
+    
+    """ Create vocab lit of the loaded dataset """
     myVocabList = createVocabList(listOPosts)
+    
+    """ calculating probabilities from word vectors"""
     trainMat=[]
     for postinDoc in listOPosts:
+        """ word vector """
         trainMat.append(setOfWords2Vec(myVocabList, postinDoc))
+    
+    """ Now let’s get the probabilities of being abusive and the two probability vectors """
     p0V,p1V,pAb = trainNB0(np.array(trainMat),np.array(listClasses))
+    
     testEntry = ['love', 'my', 'dalmation']
     thisDoc = np.array(setOfWords2Vec(myVocabList, testEntry))
     print (testEntry,'classified as: ',classifyNB(thisDoc,p0V,p1V,pAb))
+    
     testEntry = ['stupid', 'garbage']
     thisDoc = np.array(setOfWords2Vec(myVocabList, testEntry))
     print (testEntry,'classified as: ',classifyNB(thisDoc,p0V,p1V,pAb))
     
 testingNB()
+
+# Test on real world example
+def textParse(bigString):    #input is big string, #output is word list
+    import re
+    listOfTokens = re.split(r'\W+', bigString)
+    return [tok.lower() for tok in listOfTokens if len(tok) > 2]
+
+def spamTest():
+    docList = []; classList = []; fullText = []
+    for i in range(1, 26):
+        wordList = textParse(open('data/email/spam/%d.txt' % i, encoding="ISO-8859-1").read())
+        docList.append(wordList)
+        fullText.extend(wordList)
+        classList.append(1)
+        wordList = textParse(open('data/email/ham/%d.txt' % i, encoding="ISO-8859-1").read())
+        docList.append(wordList)
+        fullText.extend(wordList)
+        classList.append(0)
+    vocabList = createVocabList(docList)#create vocabulary
+    trainingSet = range(50); testSet = []           #create test set
+    for i in range(10):
+        randIndex = int(np.random.uniform(0, len(trainingSet)))
+        testSet.append(trainingSet[randIndex])
+        del(list(trainingSet)[randIndex])
+    trainMat = []; trainClasses = []
+    for docIndex in trainingSet:#train the classifier (get probs) trainNB0
+        trainMat.append(bagOfWords2VecMN(vocabList, docList[docIndex]))
+        trainClasses.append(classList[docIndex])
+    p0V, p1V, pSpam = trainNB0(np.array(trainMat), np.array(trainClasses))
+    errorCount = 0
+    for docIndex in testSet:        #classify the remaining items
+        wordVector = bagOfWords2VecMN(vocabList, docList[docIndex])
+        if classifyNB(np.array(wordVector), p0V, p1V, pSpam) != classList[docIndex]:
+            errorCount += 1
+            print("classification error", docList[docIndex])
+    print('the error rate is: ', float(errorCount)/len(testSet))
+    #return vocabList, fullText
+    
+spamTest()
+
+
 
