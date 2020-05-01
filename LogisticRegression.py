71f2b9d7e1cf4fe1cf633a08717d78fc9c0e2bdd
Gopal sharma <gopal151295@gmail.com>
2020-05-01 23:07:46 +0530

    Logistic reg | horse colic categorize

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -89,3 +89,58 @@ dataArr,labelMat=loadDataSet()
 weights = stocGradAscent1(np.array(dataArr),labelMat)
 plotBestFit(weights)
 """ """
+
+""" with different number of iterations
+weights = stocGradAscent1(np.array(dataArr),labelMat, 50)
+plotBestFit(weights)
+
+weights = stocGradAscent1(np.array(dataArr),labelMat,100)
+plotBestFit(weights)
+
+weights = stocGradAscent1(np.array(dataArr),labelMat,200)
+plotBestFit(weights)
+
+weights = stocGradAscent1(np.array(dataArr),labelMat,300)
+plotBestFit(weights)
+
+weights = stocGradAscent1(np.array(dataArr),labelMat,500)
+plotBestFit(weights)
+
+"""
+
+def classifyVector(inX, weights):
+    prob = sigmoid(sum(inX*weights))
+    if prob > 0.5: return 1.0
+    else: return 0.0
+
+def colicTest():
+    frTrain = open('data/horsecolic/horseColicTraining.txt'); frTest = open('data/horsecolic/horseColicTest.txt')
+    trainingSet = []; trainingLabels = []
+    for line in frTrain.readlines():
+        currLine = line.strip().split('\t')
+        lineArr = []
+        for i in range(21):
+            lineArr.append(float(currLine[i]))
+        trainingSet.append(lineArr)
+        trainingLabels.append(float(currLine[21]))
+    trainWeights = stocGradAscent1(np.array(trainingSet), trainingLabels, 1000)
+    errorCount = 0; numTestVec = 0.0
+    for line in frTest.readlines():
+        numTestVec += 1.0
+        currLine = line.strip().split('\t')
+        lineArr = []
+        for i in range(21):
+            lineArr.append(float(currLine[i]))
+        if int(classifyVector(np.array(lineArr), trainWeights)) != int(currLine[21]):
+            errorCount += 1
+    errorRate = (float(errorCount)/numTestVec)
+    print("the error rate of this test is: %f" % errorRate)
+    return errorRate
+
+def multiTest():
+    numTests = 10; errorSum = 0.0
+    for k in range(numTests):
+        errorSum += colicTest()
+    print("after %d iterations the average error rate is: %f" % (numTests, errorSum/float(numTests)))
+
+multiTest()
