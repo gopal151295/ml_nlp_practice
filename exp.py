c0c9b398f6efc21ade822db4ff186da923d17d01
Gopal sharma <gopal151295@gmail.com>
2020-05-02 12:27:33 +0530

    Project files refactoring

--- exp.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-mystr = "cat dog apple lion NYC love"
-random = np.random.rand(6)
-
-mystr_split = mystr.split()
-mystr_split_zip = zip(mystr_split, random)
-mystr_split_zip_list = list(mystr_split_zip)
-mystr_split_zip_list_dict = dict(mystr_split_zip_list)
